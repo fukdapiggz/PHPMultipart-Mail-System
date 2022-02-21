4873787ca2f4e790add5d6a1b4e4b701f90b80f4
UnknowenGee <38688114+fukdapiggz@users.noreply.github.com>
2022-02-21 23:22:56 +1300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<?php
+//Config
+$businessName = 'Business/YourName';
+$senderEmail = 'YourEmail@domain.com';
+$receiverName = $businessName . ' Recipient';
+$receiverEmail = 'RecipientEmail@domain.com'; //fill this here by choice
+//Links
+$InstagramLink = 'instagram.com'; //without www.
+$FacebookLink = 'facebook.com'; //without www.
+?>
+<!DOCTYPE HTML> <html><head><title>Email Form</title><meta http-equiv="content-Type" content="text/html; charset=UTF-8"><meta content="width=device-width, initial-scale=1" name="viewport" />
+<style type="text/css" title="global">.pageBox{width:100%;display:flex;justify-content:center;}.container{width:65%;}</style>
+<script type="text/javascript">
+
+function igOptionAction() {
+if (this.checked == true) {
+document.getElementById("ig").value = '1';
+document.getElementById("igLink").placeholder = "";
+document.getElementById("igLink").value = "<?php echo $InstagramLink ?>";
+document.getElementById("igLink").required = "required";
+document.getElementById("igLink").disabled = false;
+}
+else if (this.checked == false) {
+document.getElementById("ig").value = '0';
+document.getElementById("igLink").placeholder = "Enable Option to Use.";
+document.getElementById("igLink").value = "";
+document.getElementById("igLink").required = "";
+document.getElementById("igLink").disabled = true;
+}
+}
+
+function fbOptionAction() {
+if (this.checked == true) {
+document.getElementById("fb").value = '1';
+document.getElementById("fbLink").placeholder = "";
+document.getElementById("fbLink").value = "<?php echo $FacebookLink ?>";
+document.getElementById("fbLink").required = "required";
+document.getElementById("fbLink").disabled = false;
+}
+else if (this.checked == false) {
+document.getElementById("fb").value = '0';
+document.getElementById("fbLink").placeholder = "Enable Option to Use.";
+document.getElementById("fbLink").value = "";
+document.getElementById("fbLink").required = "";
+document.getElementById("fbLink").disabled = true;
+}
+}
+
+</script>
+<script type="text/javascript">
+
+function initializeToggles() {
+
+var igOption= document.getElementById('igOption');
+igOption.onchange = igOptionAction;
+
+var fbOption= document.getElementById('fbOption');
+fbOption.onchange = fbOptionAction;
+
+}
+
+</script>
+<script type="text/javascript">window.onload = initializeToggles;</script>
+	</head>
+	<body>
+<div class="pageBox">
+<form name="email" action="send_email.php" method="get" autocomplete="false" autofill="off">
+<div class="container">
+<table align="center">
+<tr><td><label for="subject"><b>Email Subject</b>:</label><br><input type="text" size="75%" id="subject" name="subject" required></td></tr>
+<tr><td><label for="businessName"><b>Sender Name</b>: *(Change in config with other fields)</label><br><input type="text" size="75%" id="businessNameDisable" name="businessNameDisable" value="<?php echo $businessName; ?>" disabled>
+<input type="hidden" id="businessName" name="businessName" value="<?php echo $businessName; ?>"></td></tr>
+<tr><td><label for="subject"><b>Sender Email</b>:</label><br><input type="text" size="75%" id="senderEmailDisable" name="senderEmailDisable" value="<?php echo $senderEmail; ?>" disabled>
+<input type="hidden" id="senderEmail" name="senderEmail" value="<?php echo $senderEmail; ?>"></td></tr>
+
+<tr><td><label for="receiverName"><b>Recipient Name</b>:</label><br><input type="text" size="75%" id="receiverName" name="receiverName" <?php if (!($receiverName ?? '') == '') {echo 'value="' . $receiverName . '"';} ?> required></td></tr>
+
+<tr><td><label for="receiverEmail"><b>Recipient Email</b>:</label><br><input type="email" size="75%" id="receiverEmail" name="receiverEmail" <?php if (!($receiverEmail ?? '') == '') {echo 'value="' . $receiverEmail . '"';} ?>  required></td></tr>
+
+<tr><td><label for="title"><b>Email Title</b>:</label><br><input type="text" size="75%" id="ti<?php if (!isset($InstagramLink)) {echo 'placeholder="Enable Option to Use."';}?> tle" name="title" required></td></tr>
+<tr><td><label for="body"><b>Email Body</b>:</label><br>
+<center><textarea style="overflow:auto;resize:none" rows="6" cols="60" id="body" name="body">
+<li><b>Bullet-point</b> one.</li>
+<li>Bullet-point two.</li>
+
+<p><b>Paragraph one.</b></p>
+<p>Paragraph two.</p>
+</textarea></center>
+</td></tr>
+<tr><td><br><center><label><b>Footer Socials</b>:</label></center></td></tr>
+<tr><center>
+<table border="1" style="float:center;min-width:535px!important;">
+	<tr>
+		<td border="1px"><center><input type="checkbox" id="igOption" <?php if (!($InstagramLink ?? '') == '') {echo "checked";} ?> >Instagram:</center></td>
+		<td border="1px"><center><input type="checkbox" id="fbOption" <?php if (!($FacebookLink ?? '') == '') {echo "checked";} ?> >Facebook:</center></td>
+	</tr>
+	<tr>
+		<td border="1px"><center><input type="hidden" id="ig" name="ig" <?php if (!($InstagramLink ?? '') == '') {echo 'value="1"';}?> ><b>"</b>https://www.
+
+			<input type="text" size="20" id="igLink" name="igLink" <?php if (!isset($InstagramLink)) {echo 'placeholder="Enable Option to Use."';}?> value="<?php echo $InstagramLink; ?>" required><b>"</b></center></td>
+		<td border="1px"><center><input type="hidden" id="fb" name="fb" <?php if (!($FacebookLink ?? '') == '') {echo 'value="1"';} ?> ><b>"</b>https://www.
+
+			<input type="text" size="20" id="fbLink" name="fbLink" <?php if (!isset($FacebookLink)) {echo 'placeholder="Enable Option to Use."';}?> value="<?php echo $FacebookLink; ?>" required><b>"</b></center></td>
+	</tr>
+</table>
+</center></tr>
+<tr><td><br><br><center><button type="submit">Submit</button></center></td></tr>
+</table>
+</div>
+</form>
+</div>
+	</body>
+</html>
---
+++ send_email.php
@@ -0,0 +1,118 @@
+<?php
+//Get info from form
+$senderName = $_GET["businessName"];
+$senderEmail = $_GET["senderEmail"];
+$receiverName = $_GET["receiverName"];
+$receiverEmail = $_GET["receiverEmail"];
+$subjectHeader = $_GET["subject"];
+$titleHeader = $_GET["title"];
+$body = $_GET["body"];
+
+
+//Subject Name
+if ($subjectHeader == $titleHeader) {
+    $chosenSubject = $titleHeader . ' | ' . $senderName;
+} else {$chosenSubject = $subjectHeader . ' | ' . $senderName;}
+
+
+//Make textonly version
+$textOnlyInput = strip_tags($body);
+$textOnly = strip_tags($body, "<strong><em>");
+
+
+//Social Footer
+if (($_GET["ig"] ?? '') == '1') {
+    $igLink = 'https://' . $_GET["igLink"];
+    $igDesktop = '<td><a href=' . $igLink . ' target="_blank"><img src="https://raw.githubusercontent.com/fukdapiggz/PHPMultipartMailSystem/main/ig.png" class="social"  /></a></td><td>&nbsp;</td>';
+} else {$igDesktop = ''; $igLink = '0';}
+
+if (($_GET["fb"] ?? '') == '1') {
+    $fbLink = 'https://' . $_GET["fbLink"];
+    $fbDesktop = '<td><a href=' . $fbLink . ' target="_blank"><img src="https://raw.githubusercontent.com/fukdapiggz/PHPMultipartMailSystem/main/fb.png" class="social"  /></a></td>';
+} else {$fbDesktop = ''; $fbLink = '0';}
+
+if (!($igLink ?? '') == '0') {$igMobile = '<a href=' . $igLink . ' class="socialLink"target="_blank">Instagram</a> &nbsp; ';} else {$igMobile = '';} if (!($fbLink ?? '') == '0') {$fbMobile = '<a href=' . $fbLink . ' class="socialLink"target="_blank">Facebook</a>';} else {$fbMobile = '';}
+
+
+//Required for multipart email
+$boundaryUID=md5(uniqid(rand()));
+$chosenBoundary = "----=_NextPart".$senderName."_" . $boundaryUID;
+$eol = "\n";
+
+
+//Fields
+$to = $receiverEmail;
+$subject = $chosenSubject;
+
+$header = "Delivered-To:".$receiverName."<".$receiverEmail.">".$eol;
+$header = "To:".$receiverName."<".$receiverEmail.">".$eol;
+$header .= "From:".$senderName."<".$senderEmail.">".$eol;
+$header .= "Cc:".$senderName."<".$senderEmail.">".$eol;
+$header .= "Return-Path:".$senderEmail.$eol;
+$header .= "X-Sender:".$senderName."<".$senderEmail.">".$eol;
+$header .= "X-Mailer: PHP/".phpversion().$eol;
+$header .= "X-Business-Group:".$senderName.$eol;
+$header .= "X-Priority: 1".$eol;
+$header .= "MIME-Version: 1.0".$eol;
+$header .= 'Content-type: multipart/alternative; boundary="'.$chosenBoundary.'"'.$eol;
+
+
+$message = "This is multipart message using MIME".$eol;
+$message .= "--".$chosenBoundary.$eol."Content-Type: text/plain; charset=UTF-8".$eol;
+$message .= "Content-Transfer-Encoding: 7bit".$eol;
+$message .= $textOnly.$eol;
+$message .= "--".$chosenBoundary.$eol."Content-type: text/html; charset=iso-8859-1".$eol;
+$message .= "Content-Transfer-Encoding: 7bit".$eol;
+$message .= '
+<html>
+  <head>
+    <title>'.$chosenSubject.'</title>
+    <meta name=3D"viewport" content=3D"width=3Ddevice-width">
+    <style type="text/css">
+      .titleHeader{font-weight:bold;font-size:1.5em;}.subHeader{color:#a6a6a6;font-style:italic;font-family:sans-serif;font-size:1em}h1{color:#0070c0;font-style:normal;font-weight:700;font-family:sans-serif;font-size:2em}body{font-style:normal;font-family:sans-serif;font-size:1em}li{margin-bottom:5px}.socialBtn{margin:25px;display:flex;justify-content:center}a.socialLink:link,a.socialLink:visited{color:#4a32a8;font-weight:700;font-size:1.5em;text-decoration:underline}a.socialLink:hover{color:#a6a6a6;font-weight:400;text-decoration:none}img.social:hover{-webkit-border-radius:10px;-moz-border-radius:10px;border-radius:10px;-webkit-box-shadow:0 0 30px 0 rgba(0,255,0,.67);-moz-box-shadow:0 0 30px 0 rgba(0,255,0,.67);box-shadow:0 0 30px 0 rgba(0,255,0,.67)}.social img.social:last-of-type:hover{-webkit-border-radius:10px;-moz-border-radius:10px;border-radius:10px;-webkit-box-shadow:0 0 20px 0 rgba(159,159,159,.67);-moz-box-shadow:0 0 20px 0 rgba(159,159,159,.67);box-shadow:0 0 20px 0 rgba(159,159,159,.67)}
+      @media only screen and (min-width:700px){.container{width:62%}.hideDesktopIcons{display:inline}.hideMobileIcons{display:none!important;visibility:hidden!important}}
+      @media only screen and (max-width:699px){.container{width:100%}.hideMobileIcons{display:inline}.hideDesktopIcons{display:none!important;visibility:hidden!important}}
+    </style>
+  </head>
+  <body>
+    <header>
+      <table>
+        <td><img src="https://raw.githubusercontent.com/fukdapiggz/PHPMultipartMailSystem/main/contactform.bmp"  /></td>
+        <td><span class="titleHeader">ContactForm</span></td>
+      </table>
+      <span class="subHeader">Easiest&nbsp;Way&nbsp;to&nbsp;Contact&nbsp;Clients.</span>
+    </header>            
+    <div class="container">
+      <hr>                
+      <center><h1>'.$subjectHeader.'</h1></center>
+      <h2>'.$titleHeader.'</h2>
+      <ul>'.$body.'</ul><br>
+      <hr>
+      <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse; padding:0; margin:0px;">
+        <tr valign="top">
+          <td align="center">
+            <div class="socialBtn">
+              <div class="hideDesktopIcons">
+                <table align="center">
+                  <tr>'.$igDesktop.' '.$fbDesktop.'</tr>
+                </table>
+              </div>
+              <div class="hideMobileIcons">'.$igMobile.' '.$fbMobile.'</div>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </body>
+</html>'.$eol;
+$message .= "--".$chosenBoundary."--";
+
+$retval = mail ($to,$subject,$message,$header);
+        
+if( $retval == true ) {
+echo 'Message sent successfully...';
+}else {
+echo 'Message could not be sent...';
+}
+echo '<br><a href="index.php">Go back to form</a>';
+?>
